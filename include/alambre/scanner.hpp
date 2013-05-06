02da472f4cee5076136ea72b0acda5270b14d075
Martin Atkins <mart@degeneration.co.uk>
2013-05-05 22:01:53 -0700

    Expand the scanner to support a few more token types.
    This is the basics of whitespace-sensitive tokenization and some initial
    reserved words. It needs a bunch more token types to fully support the
    language, and it needs to be refactored a bit so it can easily be used
    to write unit tests and to integrate it with a parser.

--- include/alambre/scanner.hpp
+++ include/alambre/scanner.hpp
@@ -15,7 +15,19 @@ namespace lex = boost::spirit::lex;
 
 enum TokenIds {
     TOK_INDENT = 256,
-    TOK_OUTDENT
+    TOK_OUTDENT,
+    TOK_IDENT,
+    TOK_IF,
+    TOK_FOR,
+    TOK_WHILE,
+    TOK_FUNC,
+    TOK_VAR,
+    TOK_CONST,
+    TOK_REQUIRE,
+    TOK_ACCEPT,
+    TOK_IMPORT,
+    TOK_FROM,
+    TOK_IN
 };
 
 template <typename Lexer>
@@ -25,21 +37,101 @@ struct alaLexer : lex::lexer<Lexer> {
 
     lex::token_def<std::string> ident;
     lex::token_def<> newline;
+    lex::token_def<> bracket;
+    lex::token_def<> space;
+    lex::token_def<> keyword;
     int indent_level;
+    char bracket_open_type;
+    int bracket_open_count;
 
-    alaLexer() : indent_level(0), ident("[a-zA-Z0-9_]+"), newline("\n *[^ ]") {
+    alaLexer() :
+        indent_level(0),
+        ident("[a-zA-Z0-9_]+", TOK_IDENT),
+        newline("\n *[^ ]"),
+        space("[ \n]+"),
+        bracket("(\\{|\\}|\\[|\\]|\\(|\\))"),
+        keyword("(if|for|while|func|var|const|require|accept|import|from|in)") {
 
         using boost::phoenix::bind;
+        using boost::phoenix::ref;
+
+        this->bracket_open_count = 0;
+        this->bracket_open_type = '\0';
 
         this->self = (
+            keyword [bind(&alaLexer::handle_keyword, this, lex::_start, lex::_end, lex::_pass, lex::_tokenid)] |
             ident |
-            //newline [cout << "Whitespace from " << lex::_start << " to " << lex::_end << "\n"]
-            newline [bind(&alaLexer::handle_whitespace, this, lex::_start, lex::_end, lex::_pass, lex::_tokenid)]
+            newline [bind(&alaLexer::handle_leading_whitespace, this, lex::_start, lex::_end, lex::_pass, lex::_tokenid)] |
+            bracket [bind(&alaLexer::handle_bracket, this, lex::_start, lex::_end, lex::_pass, lex::_tokenid)] |
+            space [bind(&alaLexer::handle_whitespace, this, lex::_start, lex::_end, lex::_pass, lex::_tokenid)]
         );
 
     }
 
+    void handle_keyword(const char*& start, const char*& end, BOOST_SCOPED_ENUM(lex::pass_flags)& pass, unsigned int& token_id) {
+
+        switch (start[0]) {
+
+            case 'f':
+                switch (start[1]) {
+                    case 'o':
+                        token_id = TOK_FOR;
+                        break;
+                    case 'r':
+                        token_id = TOK_FROM;
+                        break;
+                    case 'u':
+                        token_id = TOK_FUNC;
+                        break;
+                }
+                break;
+
+            case 'w':
+                token_id = TOK_WHILE;
+                break;
+
+            case 'v':
+                token_id = TOK_VAR;
+                break;
+
+            case 'c':
+                token_id = TOK_CONST;
+                break;
+
+            case 'r':
+                token_id = TOK_REQUIRE;
+                break;
+
+            case 'a':
+                token_id = TOK_ACCEPT;
+                break;
+
+            case 'i':
+                switch (start[1]) {
+                    case 'm':
+                        token_id = TOK_IMPORT;
+                        break;
+                    case 'n':
+                        token_id = TOK_IN;
+                        break;
+                    case 'f':
+                        token_id = TOK_IF;
+                        break;
+                }
+                break;
+
+        }
+
+    }
+
+
     void handle_whitespace(const char*& start, const char*& end, BOOST_SCOPED_ENUM(lex::pass_flags)& pass, unsigned int& token_id) {
+
+        pass = lex::pass_flags::pass_ignore;
+
+    }
+
+    void handle_leading_whitespace(const char*& start, const char*& end, BOOST_SCOPED_ENUM(lex::pass_flags)& pass, unsigned int& token_id) {
 
         // If the last character is a newline then we've found a blank
         // line, which doesn't count for indentation-detecting purposes.
@@ -53,6 +145,18 @@ struct alaLexer : lex::lexer<Lexer> {
             return;
         }
 
+        if (*(end - 1) != ' ') {
+            // Back up one so we don't eat the final non-space character.
+            end--;
+        }
+
+        // If we have a bracket open, treat leading whitespace just like
+        // any other whitespace.
+        if (this->bracket_open_count > 0) {
+            pass = lex::pass_flags::pass_ignore;
+            return;
+        }
+
         int amount = end - start - 2;
         if (amount > this->indent_level) {
             this->indent_level = amount;
@@ -62,8 +166,50 @@ struct alaLexer : lex::lexer<Lexer> {
             this->indent_level = amount;
             token_id = TOK_OUTDENT;
         }
-        // Back up one so we don't eat the final character.
-        end--;
+    }
+
+    void handle_bracket(const char*& start, const char*& end, BOOST_SCOPED_ENUM(lex::pass_flags)& pass, unsigned int& token_id) {
+
+        // We keep special track of brackets so that we can ignore
+        // indents and outdents within them.
+
+        char bracket_type = *start;
+        token_id = (unsigned int) bracket_type;
+
+        switch (bracket_type) {
+            case '(':
+            case '{':
+            case '[':
+                if (this->bracket_open_count == 0) {
+                    this->bracket_open_type = bracket_type;
+                }
+                if (this->bracket_open_type == bracket_type) {
+                    this->bracket_open_count++;
+                }
+                break;
+            case ')':
+                if (this->bracket_open_type == '(') {
+                    if (--this->bracket_open_count == 0) {
+                        this->bracket_open_type = '\0';
+                    }
+                }
+                break;
+            case '}':
+                if (this->bracket_open_type == '{') {
+                    if (--this->bracket_open_count == 0) {
+                        this->bracket_open_type = '\0';
+                    }
+                }
+                break;
+            case ']':
+                if (this->bracket_open_type == '[') {
+                    if (--this->bracket_open_count == 0) {
+                        this->bracket_open_type = '\0';
+                    }
+                }
+                break;
+        }
+
     }
 
 };
--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char **argv) {
 
     alaLexer<lexer_type> lexer;
 
-    std::string str("hello\n    hello\n\n    hello\nhello\n");
+    std::string str("hello\n    hello\n\n    hello\nif hello {\n    hello\n}\n");
     char const* first = str.c_str();
     char const* last = &first[str.size()];
 
@@ -16,16 +16,30 @@ int main(int argc, char **argv) {
     lexer_type::iterator_type end = lexer.end();
 
     while (iter != end && token_is_valid(*iter)) {
-        if ((*iter).id() == lexer.ident.id()) {
-            cout << "Ident: " << (*iter).value() << "\n";
-        }
-        if ((*iter).id() == TOK_INDENT) {
-            cout << "Indent\n";
-        }
-        if ((*iter).id() == TOK_OUTDENT) {
-            cout << "Outdent\n";
+        unsigned int token_id = (*iter).id();
+        switch (token_id) {
+            case TOK_IDENT:
+                cout << "Ident: " << (*iter).value() << "\n";
+                break;
+            case TOK_INDENT:
+                cout << "Indent\n";
+                break;
+            case TOK_OUTDENT:
+                cout << "Outdent\n";
+                break;
+            default:
+                if (token_id < 128) {
+                    cout << "Punctuation: " << (*iter).value() << "\n";
+                }
+                else {
+                    cout << "Token type " << token_id << ": " << (*iter).value() << "\n";
+                }
         }
         ++iter;
+    }
+
+    if (iter != end) {
+        cout << "Something went wrong :(\n";
     }
 
 }
