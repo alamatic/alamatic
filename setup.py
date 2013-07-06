8cf11f6df334582e37c79247e456fbe74163e950
Martin Atkins <mart@degeneration.co.uk>
2013-07-06 14:30:38 -0700

    Python project boilerplate

---
+++ setup.py
@@ -0,0 +1,9 @@
+
+from distutils.core import setup
+
+setup(
+    name="alamatic",
+    version="0.0.1",
+    description="Programming language for embedded applications",
+    packages=['alamatic'],
+)
