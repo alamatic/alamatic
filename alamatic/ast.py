edb66176fc64155c307804e940512022652261c7
Martin Atkins <mart@degeneration.co.uk>
2013-07-13 10:00:50 -0700

    Add utility for visualizing an AST, to assist in development.

--- alamatic/ast.py
+++ alamatic/ast.py
@@ -5,6 +5,26 @@ class AstNode(object):
     def __init__(self, position):
         self.position = position
 
+    @property
+    def child_nodes(self):
+        return []
+
+    @property
+    def params(self):
+        return []
+
+    def __str__(self):
+        return type(self).__name__ + "(" + (','.join(
+            (str(x) for x in self.params)
+        )) + " : " + repr(self.position) + ")"
+
+    def as_tree_rows(self, indent=0):
+        yield ("  " * indent) + "- " + str(self)
+        for child in self.child_nodes:
+            g = child.as_tree_rows(indent+1)
+            for row in g:
+                yield row
+
 
 class Module(AstNode):
 
@@ -12,6 +32,14 @@ class Module(AstNode):
         self.name = name
         self.stmts = stmts
         self.position = position
+
+    @property
+    def params(self):
+        return [self.name]
+
+    @property
+    def child_nodes(self):
+        return self.stmts
 
 
 class Statement(AstNode):
--- alamatic/tools.py
+++ alamatic/tools.py
@@ -1,3 +1,19 @@
 
+import sys
+
+
 def alac():
-    print "Hello world"
+    # For now this is acting as a parse tree vizualization tool, but of
+    # course eventually it will be the main compiler frontend.
+    from alamatic.parser import parse_module
+    from alamatic.compiler import CompileState
+    fn = sys.argv[1]
+    state = CompileState()
+    module = parse_module(
+        state,
+        file(fn),
+        None,
+        fn,
+    )
+    for row in module.as_tree_rows():
+        print row
