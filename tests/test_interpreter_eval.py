7eb491f607ffc2b091fc83433f6c9ec91427dd56
Martin Atkins <mart@degeneration.co.uk>
2013-07-27 17:37:13 -0700

    Create the state with a decorator rather than a 'with' block.
    Just looks nicer not to have the entire test function indented by one
    level.

--- tests/test_interpreter_eval.py
+++ tests/test_interpreter_eval.py
@@ -1,5 +1,6 @@
 
 import unittest
+import functools
 from alamatic.ast import *
 from alamatic.interpreter import (
     interpreter,
@@ -10,65 +11,73 @@ from alamatic.interpreter import (
 )
 
 
+def state(func=None):
+    if func is not None:
+        @functools.wraps(func)
+        def ret(self):
+            with state():
+                func(self)
+        return ret
+
+    class TestState:
+        def __init__(self, symbols, data):
+            self.symbols = symbols
+            self.data = data
+
+        def __enter__(self):
+            self.symbols.__enter__()
+            self.data.__enter__()
+
+        def __exit__(self, *args):
+            self.symbols.__exit__(*args)
+            self.data.__exit__(*args)
+
+    return TestState(
+        SymbolTable(),
+        DataState(),
+    )
+
+
 class TestInterpreterEval(unittest.TestCase):
 
-    def state(self):
-        class TestState:
-            def __init__(self, symbols, data):
-                self.symbols = symbols
-                self.data = data
+    @state
+    def test_symbol_expr(self):
+        interpreter.declare('a', 1)
+        interpreter.declare('b')
+        interpreter.declare('c')
 
-            def __enter__(self):
-                self.symbols.__enter__()
-                self.data.__enter__()
+        interpreter.mark_unknown('b', known_type=int)
 
-            def __exit__(self, *args):
-                self.symbols.__exit__(*args)
-                self.data.__exit__(*args)
+        src_a = SymbolExpr(None, "a")
+        src_b = SymbolExpr(None, "b")
+        src_c = SymbolExpr(None, "c")
+        src_d = SymbolExpr(None, "d")
 
-        return TestState(
-            SymbolTable(),
-            DataState(),
+        result_a = src_a.evaluate()
+        result_b = src_b.evaluate()
+
+        self.assertEqual(
+            type(result_a),
+            ValueExpr,
+        )
+        self.assertEqual(
+            result_a.source_node,
+            src_a,
+        )
+        self.assertEqual(
+            type(result_b),
+            SymbolStorageExpr,
+        )
+        self.assertEqual(
+            result_b.source_node,
+            src_b,
         )
 
-    def test_symbol_expr(self):
-        with self.state():
-            interpreter.declare('a', 1)
-            interpreter.declare('b')
-            interpreter.declare('c')
-
-            interpreter.mark_unknown('b', known_type=int)
-
-            src_a = SymbolExpr(None, "a")
-            src_b = SymbolExpr(None, "b")
-            src_c = SymbolExpr(None, "c")
-            src_d = SymbolExpr(None, "d")
-
-            result_a = src_a.evaluate()
-            result_b = src_b.evaluate()
-
-            self.assertEqual(
-                type(result_a),
-                ValueExpr,
-            )
-            self.assertEqual(
-                result_a.source_node,
-                src_a,
-            )
-            self.assertEqual(
-                type(result_b),
-                SymbolStorageExpr,
-            )
-            self.assertEqual(
-                result_b.source_node,
-                src_b,
-            )
-
-            self.assertRaises(
-                InconsistentTypesError,
-                src_c.evaluate,
-            )
-            self.assertRaises(
-                UnknownSymbolError,
-                src_d.evaluate,
-            )
+        self.assertRaises(
+            InconsistentTypesError,
+            src_c.evaluate,
+        )
+        self.assertRaises(
+            UnknownSymbolError,
+            src_d.evaluate,
+        )
