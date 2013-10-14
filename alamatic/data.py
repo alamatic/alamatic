fa35b1be801af5a28f0e1ef6266a021c690ee39f
Martin Atkins <mart@degeneration.co.uk>
2013-10-13 17:47:14 -0700

    Remove alamatic.data module.
    This was a temporary placeholder but the real thing is now present in
    alamatic.types.base.

--- alamatic/data.py
+++
@@ -1,7 +0,0 @@
-
-
-class Type(type):
-    # TODO: This is just a placeholder to give us something to use
-    # as a type class for now.
-    def __new__(self):
-        type.__new__("<placeholder type>", (), {})
