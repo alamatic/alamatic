8795fcc0822151fc52924bc665d3ae2e0989948e
Martin Atkins <mart@degeneration.co.uk>
2013-11-30 18:50:56 -0800

    Test intermediate code generation for DataDeclStmt.

--- tests/test_data_decl_stmt.py
+++ tests/test_data_decl_stmt.py
@@ -73,3 +73,117 @@ class TestParse(LanguageTestCase):
                 (1, 10),
             ]
         )
+
+
+class TestIntermediate(LanguageTestCase):
+
+    def test_var_no_expr(self):
+        decl = VarDeclClause(None, "foo")
+        symbols = SymbolTable()
+        self.assertIntermediateForm(
+            DataDeclStmt(
+                ('datadecl.ala', 1, 0),
+                decl,
+                None,
+            ),
+            [],
+            symbols=symbols,
+        )
+        symbol = symbols.lookup("foo")
+        self.assertFalse(
+            symbol.const,
+        )
+        self.assertEqual(
+            symbol.decl_position,
+            ('datadecl.ala', 1, 0),
+        )
+
+    def test_const_no_expr(self):
+        decl = ConstDeclClause(None, "foo")
+        stmt = DataDeclStmt(
+            None,
+            decl,
+            None,
+        )
+        elems = []
+        symbols = SymbolTable()
+        self.assertRaises(
+            NotConstantError,
+            lambda: stmt.make_intermediate_form(elems, symbols),
+        )
+
+    def test_var_expr(self):
+        decl = VarDeclClause(None, "foo")
+        symbols = SymbolTable()
+        self.assertIntermediateForm(
+            DataDeclStmt(
+                ('datadecl.ala', 1, 0),
+                decl,
+                DummyExpr("init"),
+            ),
+            [
+                ('DummyOperation', ['init']),
+                ('CopyOperation', [
+                    ('SymbolOperand', [
+                        ('NamedSymbol', 'foo'),
+                    ]),
+                    ('DummyOperand', ['init']),
+                ]),
+            ],
+            symbols=symbols,
+        )
+        symbol = symbols.lookup("foo")
+        self.assertFalse(
+            symbol.const,
+        )
+        self.assertEqual(
+            symbol.decl_position,
+            ('datadecl.ala', 1, 0),
+        )
+
+    def test_const_expr(self):
+        decl = ConstDeclClause(None, "foo")
+        symbols = SymbolTable()
+        self.assertIntermediateForm(
+            DataDeclStmt(
+                ('datadecl.ala', 1, 0),
+                decl,
+                DummyExpr("init"),
+            ),
+            [
+                ('DummyOperation', ['init']),
+                ('CopyOperation', [
+                    ('SymbolOperand', [
+                        ('NamedSymbol', 'foo'),
+                    ]),
+                    ('DummyOperand', ['init']),
+                ]),
+            ],
+            symbols=symbols,
+        )
+        symbol = symbols.lookup("foo")
+        self.assertTrue(
+            symbol.const,
+        )
+        self.assertEqual(
+            symbol.decl_position,
+            ('datadecl.ala', 1, 0),
+        )
+
+    def test_self_initialize(self):
+        # This test ensures that we correctly delay the declaration of
+        # the symbol until *after* we've evaluated its initializer, so that
+        # it's not possible to write var foo = foo where there isn't an
+        # existing foo already declared.
+        decl = DataDeclClause(None, "foo")
+        stmt = DataDeclStmt(
+            None,
+            decl,
+            SymbolNameExpr(None, "foo"),
+        )
+        elems = []
+        symbols = SymbolTable()
+        self.assertRaises(
+            UnknownSymbolError,
+            lambda: stmt.make_intermediate_form(elems, symbols),
+        )
