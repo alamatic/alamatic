1d345af6a363af9125d013230c99ce2c3079c57e
Martin Atkins <mart@degeneration.co.uk>
2013-11-30 18:33:45 -0800

    Test intermediate code generation for FuncDeclStmt.

--- tests/test_func_decl_stmt.py
+++ tests/test_func_decl_stmt.py
@@ -1,7 +1,9 @@
 
 from alamatic.ast import *
 from alamatic.types import *
+from alamatic.intermediate import *
 from alamatic.testutil import *
+from mock import MagicMock
 
 
 class TestParse(LanguageTestCase):
@@ -23,4 +25,66 @@ class TestParse(LanguageTestCase):
                     ]),
                 ]),
             ]
+        )
+
+
+class TestIntermediate(LanguageTestCase):
+
+    def test_intermediate(self):
+        decl = MagicMock
+        decl.name = 'baz'
+        block = MagicMock
+        stmt = FuncDeclStmt(
+            ('funcdecl.ala', 1, 0),
+            decl,
+            block,
+        )
+        symbols = SymbolTable()
+        elems = []
+        stmt.make_intermediate_form(elems, symbols)
+
+        self.assertEqual(
+            len(elems),
+            1,
+        )
+        elems_comp = element_comparison_nodes(elems)
+        self.assertEqual(
+            elems_comp[0][0],
+            'CopyOperation',
+        )
+        self.assertEqual(
+            elems_comp[0][1][0],
+            ('SymbolOperand', [
+                ('NamedSymbol', 'baz'),
+            ])
+        )
+        self.assertEqual(
+            elems_comp[0][1][1][0],
+            'ConstantOperand',
+        )
+        self.assertEqual(
+            type(elems_comp[0][1][1][1][0]),
+            FunctionTemplate,
+        )
+        template = elems_comp[0][1][1][1][0]
+        self.assertEqual(
+            template.decl_node,
+            stmt,
+        )
+        self.assertEqual(
+            template.decl_scope,
+            symbols,
+        )
+
+        symbol = symbols.lookup("baz")
+        self.assertEqual(
+            type(symbol),
+            NamedSymbol,
+        )
+        self.assertTrue(
+            symbol.const,
+        )
+        self.assertEqual(
+            symbol.decl_position,
+            ('funcdecl.ala', 1, 0),
         )
