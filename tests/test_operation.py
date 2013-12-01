9dca4080bab0be2ea601ea68fd7993c828ca8922
Martin Atkins <mart@degeneration.co.uk>
2013-12-01 15:41:43 -0800

    Test the generic behavior of the Operation base class.

---
+++ tests/test_operation.py
@@ -0,0 +1,28 @@
+
+from alamatic.ast import *
+from alamatic.types import *
+from alamatic.intermediate import *
+from alamatic.testutil import *
+from alamatic.codegen import CodeWriter
+
+
+class TestGenerateC(LanguageTestCase):
+
+    def test_generate_wrapper(self):
+        # For operations we have subclasses actually override
+        # _generate_c_code and then we have a standard implementation
+        # of generate_c_code that handes the invariant indent, semicolon and
+        # newline that all operations require.
+        from StringIO import StringIO
+        class DummyOperation(Operation):
+            def _generate_c_code(self, state, writer):
+                writer.write("DUMMY")
+
+        dummy = DummyOperation()
+        f = StringIO()
+        writer = CodeWriter(f)
+        dummy.generate_c_code(None, writer)
+        self.assertEqual(
+            f.getvalue(),
+            "  DUMMY;\n",
+        )
