2aead414f3fd2494e30b6c58da36f16fe7ff71f9
Martin Atkins <mart@degeneration.co.uk>
2014-08-24 17:49:40 -0700

    Define the integer types in the new type system.

--- alamatic/types/__init__.py
+++ alamatic/types/__init__.py
@@ -1,2 +1,3 @@
 
 from alamatic.types.base import *
+from alamatic.types.integer import *
---
+++ alamatic/types/integer.py
@@ -0,0 +1,57 @@
+
+from alamatic.types import TypeImplementation
+
+
+# We only export the type instances by default, since the implementations are
+# not intended to be re-used.
+__all__ = [
+    "Int64",
+    "UInt64",
+    "Int32",
+    "UInt32",
+    "Int16",
+    "UInt16",
+    "Int8",
+    "UInt8",
+    "IntUnknownSize",
+]
+
+
+class IntegerImpl(TypeImplementation):
+
+    def __init__(self, bits, signed):
+        self.bits = bits
+        self.signed = signed
+
+
+class UnknownSizeIntegerImpl(TypeImplementation):
+    """
+    Type of integer literals.
+
+    An integer literal tells us that an integer is being defined but
+    doesn't tell us the size. This type represents such an unsized integer.
+    Unsized integers support no operations except conversion to a sized
+    integer.
+
+    This is really just an implementation detail of the following idiom:
+
+    ..code-block:: alamatic
+
+        var i = 5 as UInt8
+
+    Special cases in our error reporting for unsupported operations will
+    cause a special error message to be returned if a programmer attempts to
+    use an integer literal without first converting it to a real integer type.
+    """
+    pass
+
+
+Int64 = IntegerImpl(64, True).make_no_arg_type()
+UInt64 = IntegerImpl(64, False).make_no_arg_type()
+Int32 = IntegerImpl(32, True).make_no_arg_type()
+UInt32 = IntegerImpl(32, False).make_no_arg_type()
+Int16 = IntegerImpl(16, True).make_no_arg_type()
+UInt16 = IntegerImpl(16, False).make_no_arg_type()
+Int8 = IntegerImpl(8, True).make_no_arg_type()
+UInt8 = IntegerImpl(8, False).make_no_arg_type()
+IntUnknownSize = UnknownSizeIntegerImpl().make_no_arg_type()
