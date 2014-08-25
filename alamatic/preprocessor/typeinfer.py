9601586281edf6100f86460fc154836ec6d8fea9
Martin Atkins <mart@degeneration.co.uk>
2014-08-24 18:59:57 -0700

    Basic framework for type inference.
    This gets the infrastructure in place for forward-analysis-based type
    inference, but doesn't yet actually do any inferring, yielding tables that
    indicate that all symbols have unknown types.

---
+++ alamatic/preprocessor/typeinfer.py
@@ -0,0 +1,61 @@
+
+import weakref
+import collections
+from alamatic.types import *
+
+
+__all__ = [
+    "TypeInferer",
+]
+
+
+class TypeInferer(object):
+
+    def __init__(self):
+        self._block_inferences = weakref.WeakKeyDictionary()
+
+    def infer_types_for_block(self, block):
+        old_inferences = self._block_inferences.get(block)
+        inferences = TypeTable()
+        for pred_block in block.predecessors:
+            if pred_block in self._block_inferences:
+                inferences.merge(self._block_inferences[pred_block])
+            else:
+                # Until our predecessors have run, just produce an empty
+                # table signalling that all of our types are unknown.
+                self._block_inferences[block] = TypeTable()
+                # Tell the driver that we changed nothing so that we won't get
+                # run again yet. This predecessor will eventually run and
+                # produce a result and then we'll get re-queued.
+                return False
+
+        # TODO: Actually do some type inference.
+
+        self._block_inferences[block] = inferences
+        return False
+
+    def get_inferences_for_block(self, block):
+        return self._block_inferences.get(block, TypeTable())
+
+
+class TypeTable(object):
+
+    def __init__(self):
+        self._symbol_types = collections.defaultdict(
+            lambda x: TypeConstructor().instantiate()
+        )
+
+    def merge(self, other):
+        for symbol, other_type in other._symbol_types.iteritems():
+            if symbol in self._symbol_types:
+                self._symbol_types[symbol] = self._symbol_types[symbol].merge(
+                    other_type,
+                )
+            else:
+                self._symbol_types[symbol] = other_type
+
+    def __getitem__(self, key):
+        return self._symbol_types[key]
+
+    def __eq__(self, other):
+        return self._symbol_types == other._symbol_types
