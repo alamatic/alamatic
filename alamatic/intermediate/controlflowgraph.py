61a6cc98cb607beb7a02bf02990f2646d0af527d
Martin Atkins <mart@degeneration.co.uk>
2014-11-22 19:28:33 -0800

    Property to enumerate all of the blocks in a CFG.

--- alamatic/intermediate/controlflowgraph.py
+++ alamatic/intermediate/controlflowgraph.py
@@ -9,3 +9,33 @@ class ControlFlowGraph(object):
 
     def create_block(self):
         return BasicBlock(self)
+
+    @property
+    def blocks(self):
+        """
+        Iterable of all blocks reachable from the entry block.
+        """
+        # Keep track of what we've already emitted so we don't return
+        # the same block twice if there's a loop or diamond.
+        blocks_emitted = set()
+
+        # Keep track of blocks that are targets of jumps we've encountered
+        # so we can emit them in a sensible order.
+        block_stack = []
+
+        current_block = self.entry_block
+        while True:
+            if current_block not in blocks_emitted:
+                yield current_block
+
+            blocks_emitted.add(current_block)
+
+            terminator = current_block.terminator
+            assert terminator is not None
+
+            block_stack.extend(reversed(list(terminator.successor_blocks)))
+
+            if len(block_stack) > 0:
+                current_block = block_stack.pop()
+            else:
+                break
