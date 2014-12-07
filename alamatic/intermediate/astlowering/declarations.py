7ff1b160b110f49d867a9c80f59251b851218dc9
Martin Atkins <mart@degeneration.co.uk>
2014-12-06 21:32:02 -0800

    AST lowering for the declaration of constants.

--- alamatic/intermediate/astlowering/declarations.py
+++ alamatic/intermediate/astlowering/declarations.py
@@ -15,3 +15,8 @@ def lower_declaration(decl, scope, builder):
 @lower_declaration.overload(VarDeclClause)
 def lower_var_decl_clause(decl, scope, builder):
     return scope.declare_variable(decl.name, decl.source_range)
+
+
+@lower_declaration.overload(ConstDeclClause)
+def lower_const_decl_clause(decl, scope, builder):
+    return scope.declare_constant(decl.name, decl.source_range)
--- alamatic/intermediate/program.py
+++ alamatic/intermediate/program.py
@@ -37,6 +37,41 @@ class GlobalVariable(Symbol):
         return self.codegen_name
 
 
+class Constant(Symbol):
+
+    def __init__(self, index, decl_name, decl_range):
+        self.index = index
+        Symbol.__init__(self, decl_name, decl_range)
+
+    @property
+    def codegen_name(self):
+        return "const_%03x" % self.index
+
+    def make_ir_load(self, builder, source_range=None):
+        return builder.load_constant(
+            location=self,
+            source_range=source_range,
+        )
+
+    def make_ir_store(self, builder, value, source_range=None):
+        builder.store_constant(
+            location=self,
+            source=value,
+            source_range=source_range,
+        )
+
+    def __repr__(self):
+        return "<Constant %s for %s at %s>" % (
+            self.codegen_name,
+            self.decl_name,
+            self.decl_range,
+        )
+
+    @property
+    def as_debug_value(self):
+        return self.codegen_name
+
+
 class Program(object):
     """
     An IR program.
@@ -49,12 +84,11 @@ class Program(object):
 
     def __init__(self, entry_func):
         self.global_variables = []
+        self.constants = []
         self.entry_func = entry_func
         self.root_scope = Scope(
             variable_cons=self.declare_variable,
-
-            # TODO: Need a constant constructor too.
-            constant_cons=None,
+            constant_cons=self.declare_constant,
         )
 
     def declare_variable(self, decl_name, decl_range):
@@ -62,3 +96,9 @@ class Program(object):
         variable = GlobalVariable(index, decl_name, decl_range)
         self.global_variables.append(variable)
         return variable
+
+    def declare_constant(self, decl_name, decl_range):
+        index = len(self.constants)
+        constant = Constant(index, decl_name, decl_range)
+        self.constants.append(constant)
+        return constant
